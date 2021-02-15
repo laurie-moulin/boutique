e4af0fb3e4a021ced51956f82dc429dd7d01ca3f
aicha <aicha.ouattara@laplateforme.io>
2021-02-15 13:02:53 +0100

    Boutique ok

--- pages/commande.php
+++ pages/commande.php
@@ -21,12 +21,12 @@ $commands = new \db\Commands();
 
 if (isset($_POST["payer"]))
 {
-    $commands->insertcommande(31, $commands->montant(),date('Y-m-d'));
+    $commands->insertcommande(32, $commands->montant(),date('Y-m-d'));
     $lastID = $commands->lastInsertId();
 
     foreach($_SESSION["panier"] as $keys => $values)
     {
-        $commands->insertcommandedetail($lastID,31,$values["item_id"], $values["item_quantity"], $values["item_price"], $values["item_size"]);
+        $commands->insertcommandedetail($lastID,32,$values["item_id"], $values["item_quantity"], $values["item_price"], $values["item_size"]);
     }
     $commands->UpdateStock($values["item_quantity"], $values["item_id"], $values["item_size"]);
     header('location:paiement_paypal.php');
